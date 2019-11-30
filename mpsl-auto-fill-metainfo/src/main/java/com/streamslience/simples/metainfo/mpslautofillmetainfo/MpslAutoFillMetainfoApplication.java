d887b76a81794e7ad0913c8667b0bff1514136c6
StreamSlience <1452909091@qq.com>
2019-11-30 20:21:22 +0800

    优化

---
+++ mpsl-auto-fill-metainfo/src/main/java/com/streamslience/simples/metainfo/mpslautofillmetainfo/MpslAutoFillMetainfoApplication.java
@@ -0,0 +1,13 @@
+package com.streamslience.simples.metainfo.mpslautofillmetainfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpslAutoFillMetainfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpslAutoFillMetainfoApplication.class, args);
+    }
+
+}
