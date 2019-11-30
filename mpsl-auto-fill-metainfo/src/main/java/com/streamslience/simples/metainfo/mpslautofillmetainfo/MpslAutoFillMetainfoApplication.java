8047bf4078d48995323b88c28a85359e59de2ab0
StreamSlience <1452909091@qq.com>
2019-12-01 00:54:48 +0800

    新增字段注入案例(README.md没有整理)

--- mpsl-auto-fill-metainfo/src/main/java/com/streamslience/simples/metainfo/mpslautofillmetainfo/MpslAutoFillMetainfoApplication.java
+++ mpsl-auto-fill-metainfo/src/main/java/com/streamslience/simples/metainfo/mpslautofillmetainfo/MpslAutoFillMetainfoApplication.java
@@ -1,9 +1,11 @@
 package com.streamslience.simples.metainfo.mpslautofillmetainfo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.streamslience.simples.metainfo.mpslautofillmetainfo.dao")
 public class MpslAutoFillMetainfoApplication {
 
     public static void main(String[] args) {
