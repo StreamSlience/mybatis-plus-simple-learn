efa39271a432a825533909251ae3c764bead8f42
沈泽庆 <1452909091@qq.com>
2019-12-01 20:27:43 +0800

    新增攻击SQL阻断解析器案例

---
+++ mpsl-sql-parser/src/main/java/com/streamslience/simples/sqlparser/mpslsqlparser/MpslSqlParserApplication.java
@@ -0,0 +1,13 @@
+package com.streamslience.simples.sqlparser.mpslsqlparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpslSqlParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpslSqlParserApplication.class, args);
+    }
+
+}
---
+++ mpsl-sql-parser/src/test/java/com/streamslience/simples/sqlparser/mpslsqlparser/MpslSqlParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamslience.simples.sqlparser.mpslsqlparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpslSqlParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
