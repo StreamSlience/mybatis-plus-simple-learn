6bc6afe34d3c814c56f4ca5a1966ca4c73780901
沈泽庆 <1452909091@qq.com>
2019-12-01 22:13:52 +0800

    新增CRUD内置操作案例

---
+++ mpsl-crud/src/main/java/com/streamslience/simples/crud/mpslcrud/MpslCrudApplication.java
@@ -0,0 +1,13 @@
+package com.streamslience.simples.crud.mpslcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpslCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpslCrudApplication.class, args);
+    }
+
+}
---
+++ mpsl-crud/src/test/java/com/streamslience/simples/crud/mpslcrud/MpslCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamslience.simples.crud.mpslcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpslCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
