73312dffd0b4ace4424a75be5ed6e1318c6eed48
沈泽庆 <1452909091@qq.com>
2019-12-02 10:11:23 +0800

    123

--- mpsl-crud/src/test/java/com/streamslience/simples/crud/mpslcrud/MpslCrudApplicationTests.java
+++ mpsl-crud/src/test/java/com/streamslience/simples/crud/mpslcrud/MpslCrudApplicationTests.java
@@ -1,8 +1,10 @@
 package com.streamslience.simples.crud.mpslcrud;
 
+import com.streamslience.simples.crud.mpslcrud.dao.UserDao;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,9 +13,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 class MpslCrudApplicationTests {
 
+    @Autowired
+    private UserDao userDao;
+
     @Test
     void contextLoads() {
     }
 
+    @Test
+    public void aInsert() {
+
+    }
 
 }
