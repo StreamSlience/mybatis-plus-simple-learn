a83d6ba507b0846b43800f8e3b7ce5e2817e881f
沈泽庆 <1452909091@qq.com>
2019-12-03 16:02:15 +0800

    新增乐观锁插件案例

---
+++ mpsl-optimistic-locker/src/main/java/com/streamslience/simples/optimisticlocker/mpsloptimisticlocker/MpslOptimisticLockerApplication.java
@@ -0,0 +1,13 @@
+package com.streamslience.simples.optimisticlocker.mpsloptimisticlocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpslOptimisticLockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpslOptimisticLockerApplication.class, args);
+    }
+
+}
---
+++ mpsl-optimistic-locker/src/test/java/com/streamslience/simples/optimisticlocker/mpsloptimisticlocker/MpslOptimisticLockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamslience.simples.optimisticlocker.mpsloptimisticlocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpslOptimisticLockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
