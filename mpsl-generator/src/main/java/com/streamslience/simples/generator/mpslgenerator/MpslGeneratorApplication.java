0f55ee3b319c09ed36bd6f6f000ecd23926f8474
沈泽庆 <1452909091@qq.com>
2019-12-10 20:44:52 +0800

    新增代码自动生成案例, 模版引擎待写完

---
+++ mpsl-generator/src/main/java/com/streamslience/simples/generator/mpslgenerator/MpslGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.streamslience.simples.generator.mpslgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpslGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpslGeneratorApplication.class, args);
+    }
+
+}
